dd83c6be531c3b226868825ba9ecb3ad0f729931
abhijeet <abhijeetkoli39@gamil.com>
2024-06-10 15:06:10 +0530

    new

--- Server.js
+++ Server.js
@@ -84,7 +84,7 @@ app.post("/forgot-password", async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested to reset the password for your account.\n\n
              Please click on the following link, or paste this into your browser to complete the process:\n\n
-             https://starwars-frontend.onrender.com/${token}\n\n
+             https://starwars-frontend.onrender.com/reset-password/${token}\n\n
              If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
